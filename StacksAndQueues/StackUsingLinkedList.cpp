e996e2283ca235810002e1ed73b26020a8efcad1
Pranjal Yadav <99741190+pranjalyadav-py@users.noreply.github.com>
2024-03-09 17:43:21 +0530

    commit

--- StacksAndQueues/StackUsingLinkedList.cpp
+++ StacksAndQueues/StackUsingLinkedList.cpp
@@ -3,12 +3,66 @@ using namespace std;
 
 // Dynamically: Using a linked list
 
+// its a node only
 class StackNode{
     int data;
     StackNode * next;
     int size;
 
     public:
+    StackNode(int x){
+        data = x;
+        next = NULL;
+    }
+};
 
-    
+// have the top 
+Class Stack{
+
+    StackNode* top;
+    int size;
+
+    // constructor
+    Stack()
+    {
+        top = NULL;
+        size = 0;
+    }
+
+    void push(int x)
+    {
+        // make a new element to store x
+        StackNode* element = new StackNode(x); // its next points to null
+        element->next = top();
+        top = element;
+        size++;
+    }
+
+    int pop()
+    {
+        // check for underflow
+        if(top == NULL)
+        return -1;
+
+        int topData  = top->data;
+
+        StackNode temp = top;
+        top = top -> next;
+        delete temp;
+
+        //maintain size dude
+        size--;
+
+        return topData;
+    }
+
+    int StackSize()
+    {
+        return size;
+    }
+
+    bool StackIsEmpty()
+    {
+        return top == NULL
+    }
 }
